30aef11425737b36e9db49b3c4b0839408461281
vasanthbharathi07 <vasanthbharathi07@gmail.com>
2024-09-29 14:33:45 +0530

    Addressed mypy errors

--- src/services/extract_image_services.py
+++ src/services/extract_image_services.py
@@ -1,7 +1,7 @@
-from fastapi import File
-from services.pdf_service.pdf_extraction_service import extract_images_from_pdf
+from fastapi import File, UploadFile
+from src.services.pdf_service.pdf_extraction_service import extract_images_from_pdf
 
 
-def extract_image_service(file_extension,file:File,width,height):
+def extract_image_service(file_extension,file:UploadFile = File(...),width=None,height=None):
     if file_extension == ".pdf":
         return extract_images_from_pdf(file,width,height)
